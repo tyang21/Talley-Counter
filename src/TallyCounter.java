2c84c30d6c23d8a4d43907c28c304caef0697f90
Tyler <Tyler@GamePC.fios-router.home>
2018-09-25 23:35:48 -0400

    ayyyyyy

--- src/TallyCounter.java
+++ src/TallyCounter.java
@@ -1,4 +1,36 @@
-
-public class TallyCounter {
-
+/**
+ * 
+ * @author Tyler
+ *
+ */
+public class TallyCounter 
+{
+	private int count;
+	
+	public TallyCounter() 
+	{
+		count = 0;
+	}
+	public TallyCounter(int startNum) 
+	{
+		count = startNum;
+	}
+	public void click() 
+	{
+		count = count + 1;
+		// Can also be count++
+		// As well as count +=1
+	}
+	public void reset() 
+	{
+		count = 0;
+	}
+	public int getCount() 
+	{
+		return count;
+	}
+	public void unclick() 
+	{	
+		count--;
+	}
 }
